365b5e5a1d1775ed6f74ae1b7add36a604b3c5c5
Revathi R <revathi17r@gmail.com>
2024-04-09 18:25:43 +0530

    Hello World! in CPP

---
+++ 1_Hello_world.cpp
@@ -0,0 +1,12 @@
+// Hackerrank 1- https://www.hackerrank.com/challenges/cpp-hello-world?isFullScreen=true
+
+//Solution
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
